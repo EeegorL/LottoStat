fd072446fb280a12f8c20656632a0def5a67d7bd
EeegorL <egorfinn@gmail.com>
2024-01-16 19:24:03 +0200

    LottoStat

---
+++ src/main/java/LottoStat/LottoStat.java
@@ -0,0 +1,205 @@
+package LottoStat;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import org.apache.poi.xssf.usermodel.*;
+
+public class LottoStat {
+    public static SheetHandler handler;
+    public static JTable resultTable;
+    public static TableModel resultTableModel;
+    public static ArrayList<String> columns;
+    public static JFrame mainFrame;
+    public static int languageInt; // 0 = finnish, 1 = english
+
+    public static String[] inputText = {"Syötä tulos pilkuilla erotettuna, esim. 1,2,30,4,531,6", "Insert result separated by commas, e.g. 1,2,30,4,531,6"};
+    public static String[] sendText = {"Lähetä", "Send"};
+    public static String[] langText = {"Vaihda kieltä", "Change language"};
+    public static String[] wrongFormatText = {"Väärä muoto, tarkista syöte. Vain numerot (1,2,3,4...) ja pilkut (,) sallittuja", "Wrong format, check your input. Only numbers( 1,2,3,4...) and commas (,) allowed"};
+    public static String[] addSuccessText = {"Lisätty", "Added"};
+    public static String[] eraseButtonText = {"Poista kaikki", "Erase all"};
+    public static String[] deleteSuccessText = {"Poistettu", "Deleted"};
+
+
+    public static void main(String[] args) {
+        try {
+            File excelFile = new File("src/main/java/LottoStat/stats.xlsx");
+            FileInputStream excelFileStream = new FileInputStream(excelFile);
+            XSSFWorkbook excelHandler = new XSSFWorkbook(excelFileStream);
+            SheetHandler sheetHandler = new SheetHandler(excelHandler);
+
+            handler = sheetHandler;
+            languageInt = 0;
+
+            swing();
+        } catch (FileNotFoundException err) {
+            throw new Error(err);
+        } catch (IOException err) {
+            throw new Error(err);
+        } catch (Error err) {
+            throw new Error(err);
+        }
+    }
+
+    public static void swing() {
+        mainFrame = new JFrame("LottoStat");
+
+        mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        mainFrame.setSize(1300, 600);
+        mainFrame.setLayout(null);
+        mainFrame.setVisible(true);
+        mainFrame.setResizable(false);
+
+        JTextArea insert = new JTextArea();
+        JLabel insertTitle = new JLabel(inputText[languageInt]);
+
+        // button
+        JButton submitButton = new JButton(sendText[languageInt]);
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String insertValue = insert.getText();          
+                
+                if(Pattern.matches("^[0-9,]*$", insertValue)) {
+                    for(String value : insertValue.split(",")) {
+                        try {
+                            handler.incrementOrCreate(Integer.valueOf(value));
+                            
+                            resultTable.setModel(new DefaultTableModel(handler.getAllRows(), columns.toArray()));
+                        }
+                        catch (NumberFormatException e1) {} 
+                        catch (IOException e1) {}
+                    }
+                    showStatusMsg(addSuccessText[languageInt], false);
+                } 
+                else showStatusMsg(wrongFormatText[languageInt], true);
+            }
+        });
+        
+        JButton eraseDataButton = new JButton(eraseButtonText[languageInt]);
+        eraseDataButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    handler.eraseData();
+                    resultTable.setModel(new DefaultTableModel(handler.getAllRows(), columns.toArray()));
+                    showStatusMsg(deleteSuccessText[languageInt], false);
+                } catch (IOException e1) {
+                    showStatusMsg("Virhe/Error", true);
+                    throw new Error(e1);
+                }
+            }
+        });
+        JButton langButton = new JButton(langText[languageInt]);
+        langButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                languageInt = languageInt == 1 ? 0 : 1;
+
+                insertTitle.setText(inputText[languageInt]);
+                submitButton.setText(sendText[languageInt]);
+                langButton.setText(langText[languageInt]); 
+                eraseDataButton.setText(eraseButtonText[languageInt]); 
+            }
+        });
+
+
+
+        // result table
+        columns = new ArrayList<String>();
+        columns.add("Num");
+        columns.add("x");
+        resultTableModel = new DefaultTableModel(handler.getAllRows(), columns.toArray());
+ 
+        resultTable = new JTable(resultTableModel) {
+            public boolean isCellEditable(int row, int column) {                
+                    return false;               
+            };
+        };
+        JScrollPane scrollPane = new JScrollPane(resultTable);
+
+        JLabel credits = new JLabel("Mikko Egor Legezin, 2024");
+
+        // bounds
+        insert.setBounds(10, 50, 1000, 30);
+        insertTitle.setBounds(10, 20, 1000, 30);
+        submitButton.setBounds(10, 90, 100, 30);
+        scrollPane.setBounds(10, 130, 400, 300);
+        langButton.setBounds(1050, 15, 100,25);
+        credits.setBounds(0, mainFrame.getHeight() - 64, 1300, 35);
+        eraseDataButton.setBounds(1020, credits.getY() - 30, eraseDataButton.getText().length() * 15,25);
+
+        // pushing
+        mainFrame.add(insert);
+        mainFrame.add(insertTitle);
+        mainFrame.add(submitButton);
+        mainFrame.add(scrollPane);
+        mainFrame.add(langButton);
+        mainFrame.add(credits);
+        mainFrame.add(eraseDataButton);
+
+
+        // styles
+        insert.setBackground(Color.white);
+        insert.setForeground(Color.black);
+        credits.setOpaque(true);
+        credits.setBackground(Color.black);
+        credits.setForeground(Color.white);
+        insert.setBorder(BorderFactory.createLineBorder(Color.orange));
+        mainFrame.getContentPane().setBackground(Color.WHITE);
+        insert.setFont(new Font("Serif", Font.PLAIN, 20));
+        insertTitle.setFont(new Font("Serif", Font.BOLD, 25));
+        submitButton.setFont(new Font("Serif", Font.BOLD, 20));
+        submitButton.setFocusPainted(false);
+        submitButton.setContentAreaFilled(false);
+        langButton.setFont(new Font("Serif", Font.BOLD, 10));
+        langButton.setFocusPainted(false);
+        langButton.setContentAreaFilled(false);
+        langButton.setMargin(new Insets(0, 0, 0, 0));
+
+        eraseDataButton.setFont(new Font("Serif", Font.BOLD, 20));
+        eraseDataButton.setFocusPainted(false);
+        eraseDataButton.setContentAreaFilled(false);
+        eraseDataButton.setForeground(Color.RED);
+        eraseDataButton.setBorder(BorderFactory.createLineBorder(Color.RED));
+
+        //table styles
+        resultTable.setFont(new Font("Serif", Font.PLAIN, 17));
+        resultTable.setRowHeight(20);
+        resultTable.setRowMargin(5);
+
+        mainFrame.repaint();
+    }
+
+    public static void showStatusMsg(String msg, boolean isError) {
+        JLabel info = new JLabel(msg);
+        info.setBounds(150, 90, info.getText().length() * 10,30);
+        info.setHorizontalAlignment(JLabel.CENTER);
+        
+        info.setBorder(BorderFactory.createLineBorder(isError == true ? Color.RED : Color.GREEN, 5));
+
+        mainFrame.add(info);
+        mainFrame.repaint();
+
+        new java.util.Timer().schedule( 
+            new java.util.TimerTask() {
+                @Override
+                public void run() {
+                    mainFrame.remove(info);
+                    mainFrame.repaint();
+                }
+            }, 1500
+    );
+    }
+}
---
+++ src/main/java/LottoStat/SheetHandler.java
@@ -0,0 +1,108 @@
+package LottoStat;
+
+import org.apache.poi.ss.usermodel.Row;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class SheetHandler {
+    private XSSFWorkbook excel;
+    private Sheet table;
+
+    public SheetHandler(XSSFWorkbook excel) {
+        this.excel = excel;
+        this.table = excel.getSheet("LottoStat");
+    }
+
+    public Object[][] getAllRows() {
+        ArrayList<String[]> values = new ArrayList<String[]>();
+
+        for (int i = 0; i < table.getPhysicalNumberOfRows(); i++) {
+            Row row = table.getRow(i);
+            values.add(new String[] {String.valueOf((int)row.getCell(0).getNumericCellValue()), String.valueOf((int)row.getCell(1).getNumericCellValue())});
+        }
+        
+        Collections.sort(values, new Comparator<String[]>() { //sorts the values descendingly
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                return Integer.valueOf(o2[1]).compareTo(Integer.valueOf(o1[1]));
+            }
+        });
+
+        return values.toArray(new Object[][] {});
+    }
+
+    public P numberExists(int number) {
+        for (int i = 0; i < table.getPhysicalNumberOfRows(); i++) {
+            Row row = table.getRow(i);
+            Cell titleCell = row.getCell(0);
+
+            if ((int) titleCell.getNumericCellValue() == number)
+                return new P(true, i);
+        }
+        return new P(false, 0);
+    }
+
+    public void incrementOrCreate(int value) throws IOException {
+        P rowWithValue = numberExists(value);
+
+        if (rowWithValue.exists()) {
+            Cell foundCell = table.getRow(rowWithValue.rowNum()).getCell(1);
+            foundCell.setCellValue(foundCell.getNumericCellValue() + 1);
+            update();
+        } else {
+            table.createRow(table.getPhysicalNumberOfRows()); // counting starts from 1 unlike index, so no need to ++
+            Row createdRow = table.getRow(table.getPhysicalNumberOfRows() - 1);
+
+            createdRow.createCell(0);
+            createdRow.createCell(1);
+            createdRow.getCell(0).setCellValue((double) value);
+            createdRow.getCell(1).setCellValue(0);
+
+            incrementOrCreate(value); // re-runs the function and finds the now created column in the if-exists, incrementing it by one
+        }
+    }
+
+    public void update() throws IOException {
+        FileOutputStream outFile = new FileOutputStream(
+                new File("src/main/java/LottoStat/stats.xlsx"));
+        excel.write(outFile);
+        outFile.close();
+    }
+
+    public void eraseData() throws IOException {
+        if(table.getPhysicalNumberOfRows() > 0) {
+            for(int i=0; i<= table.getLastRowNum(); i++){
+                Row row = table.getRow(i);
+                table.removeRow(row);
+            }
+        }
+        update();
+    }
+}
+
+class P {
+    private boolean exists;
+    private int rowNum;
+
+    P(boolean exists, int rowNum) {
+        this.exists = exists;
+        this.rowNum = rowNum;
+    }
+
+    boolean exists() {
+        return this.exists;
+    };
+
+    int rowNum() {
+        return this.rowNum;
+    };
+}
